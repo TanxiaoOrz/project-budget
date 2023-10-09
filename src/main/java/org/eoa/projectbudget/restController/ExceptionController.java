e7a6718bd20689c828182a9a179cc4dc0006547b
Tanxiao <13671985248@163.com>
2023-10-09 17:23:17 +0800

    增加唯一值约束异常统一处理

--- src/main/java/org/eoa/projectbudget/restController/ExceptionController.java
+++ src/main/java/org/eoa/projectbudget/restController/ExceptionController.java
@@ -2,10 +2,14 @@ package org.eoa.projectbudget.restController;
 
 import lombok.extern.slf4j.Slf4j;
 import org.eoa.projectbudget.exception.EoaException;
+import org.eoa.projectbudget.exception.ParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import org.eoa.projectbudget.vo.Vo;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
 
 /**
  * @Author 张骏山
@@ -19,6 +23,16 @@ import org.eoa.projectbudget.vo.Vo;
 @RestControllerAdvice
 @Slf4j
 public class ExceptionController {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Vo<String> handleViolateUniqueConstraint(SQLIntegrityConstraintViolationException e) {
+        String[] strings = (String[]) Arrays.stream(
+                e.getMessage().split(" "))
+                .filter(s -> s.startsWith("'")).toArray();
+        String attribute = strings[0];
+        String value = strings[1];
+        return new Vo<>(new ParameterException(attribute,value,e.getMessage()));
+    }
 
     @ExceptionHandler(EoaException.class)
     public Vo<String> handleEoaException(EoaException e) {
