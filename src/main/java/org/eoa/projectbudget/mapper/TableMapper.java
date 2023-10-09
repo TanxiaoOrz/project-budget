ed5ac243f81ddbb7111aa07b6703cd393d5ba599
Tanxiao <13671985248@163.com>
2023-10-09 17:51:54 +0800

    表单实体操作mapper声明

---
+++ src/main/java/org/eoa/projectbudget/mapper/TableMapper.java
@@ -0,0 +1,16 @@
+package org.eoa.projectbudget.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.eoa.projectbudget.entity.Table;
+
+/**
+ * @Author 张骏山
+ * @Date 2023/10/9 13:57
+ * @PackageName: org.eoa.projectbudget.mapper
+ * @ClassName: TableMapper
+ * @Description: TODO
+ * @Version 1.0
+ */
+public interface TableMapper extends BaseMapper<Table> {
+
+}
