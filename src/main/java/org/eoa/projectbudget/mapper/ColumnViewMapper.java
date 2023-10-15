50d1ab1f2cc89061209e12e52f2bc851f3ca1088
Tanxiao <13671985248@163.com>
2023-10-15 23:02:58 +0800

    ColumnMapper声明和获取方法

---
+++ src/main/java/org/eoa/projectbudget/mapper/ColumnViewMapper.java
@@ -0,0 +1,19 @@
+package org.eoa.projectbudget.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.eoa.projectbudget.entity.ColumnView;
+
+@Mapper
+public interface ColumnViewMapper extends BaseMapper<ColumnView> {
+
+    /**
+     * 通过字段所属表单查询字段显示编号应赋值
+     * @param tableNo 字段所属表单编号
+     * @return 字段显示最新值
+     */
+    @Select("select ifnull(max(columnViewNo),0)+1 from table_view_column_index where tableNo = #{tableNo}")
+    Integer getColumnViewNoNew(@Param("tableNo")Long tableNo);
+}
