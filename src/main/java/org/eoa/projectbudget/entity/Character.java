b1ba2aadaf897df964c872017c0c490a43733b61
Tanxiao <13671985248@163.com>
2023-10-27 17:33:44 +0800

    权限处理类

---
+++ src/main/java/org/eoa/projectbudget/entity/Character.java
@@ -0,0 +1,12 @@
+package org.eoa.projectbudget.entity;
+
+/**
+ * @Author 张骏山
+ * @Date 2023/10/27 17:20
+ * @PackageName: org.eoa.projectbudget.entity
+ * @ClassName: Character
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Character {
+}
---
+++ src/main/java/org/eoa/projectbudget/utils/AuthorityUtils.java
@@ -0,0 +1,30 @@
+package org.eoa.projectbudget.utils;
+
+/**
+ * @Author 张骏山
+ * @Date 2023/10/27 16:50
+ * @PackageName: org.eoa.projectbudget.utils
+ * @ClassName: AuthorityUtils
+ * @Description: 权限处理工具
+ * @Version 1.0
+ */
+public class AuthorityUtils {
+    static class Creator {
+
+    }
+
+    static class Character {
+
+    }
+
+    /**
+     * 不一定使用
+     */
+    static class Matrix {
+
+    }
+
+    static class Proposed {
+
+    }
+}
