92bc13d1be5b53f9ce76291dac08ea53a8a44ae0
Tanxiao <13671985248@163.com>
2023-10-30 15:47:32 +0800

    缓存service完成

---
+++ src/main/java/org/eoa/projectbudget/service/cache/CacheServiceImpl.java
@@ -0,0 +1,113 @@
+package org.eoa.projectbudget.service.cache;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eoa.projectbudget.exception.DataException;
+import org.eoa.projectbudget.exception.EoaException;
+import org.eoa.projectbudget.exception.ServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2023/10/30 13:46
+ * @PackageName: org.eoa.projectbudget.cache
+ * @ClassName: BaseCache
+ * @Description: redis缓存直接操作类
+ * @Version 1.0
+ **/
+
+@Service
+public class CacheServiceImpl {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private final Logger log = LoggerFactory.getLogger("CacheModule");
+
+    public CacheServiceImpl flashFlagCache(String flag) {
+
+        Boolean delete = redisTemplate.delete(flag);
+        log.info("删除:Flag=>{}的所有缓存:success=>{}",flag,delete);
+        return this;
+    }
+
+    public <T> T  getCache(String flag, String method,String userId, Date changeFlag,Class<T> clazz) throws EoaException {
+        String key = String.format("%s-%s", userId, method);
+        WithTime withTime = (WithTime) redisTemplate.boundHashOps(flag).get(key);
+        if (withTime == null) {
+            log.info("获取:Flag=>{}更新时间=>{}的缓存:=>{}success=>false",flag,changeFlag,key);
+            return null;
+        }
+        log.info("获取:Flag=>{}更新时间=>{}的缓存:=>{}success=>true",flag,changeFlag,key);
+        String s = (withTime).valueEffective(changeFlag);
+        try {
+            return new ObjectMapper().readValue(s,clazz);
+        } catch (JsonProcessingException e) {
+            throw new DataException("redis",flag,key,s);
+        }
+    }
+
+    public CacheServiceImpl setCache(String flag, String method, String userId, Object object) throws EoaException {
+        String key = String.format("%s-%s", userId, method);
+        String value;
+        try {
+            value = new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.info("存储:Flag=>{}的缓存:=>{}success=>false",flag,key);
+            throw new ServerException("数据转换错误", e);
+        }
+        BoundHashOperations<String, Object, Object> hashFlag = redisTemplate.boundHashOps(flag);
+        if (Boolean.TRUE.equals(hashFlag.hasKey(key)))
+            hashFlag.delete(key);
+        hashFlag.put(key, new WithTime(value));
+        log.info("存储:Flag=>{}的缓存:=>{}success=>true",flag,key);
+        return this;
+    }
+
+    private static class WithTime {
+        String object;
+        Date date;
+
+        String valueEffective(Date changeFlag) {
+            if (date.after(changeFlag))
+                return object;
+            else
+                return null;
+        }
+
+        public WithTime() {
+        }
+
+        public WithTime(String object) {
+            this.object = object;
+            this.date = new Date();
+        }
+
+        public Object getObject() {
+            return object;
+        }
+
+        public WithTime setObject(String object) {
+            this.object = object;
+            return this;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public WithTime setDate(Date date) {
+            this.date = date;
+            return this;
+        }
+    }
+
+
+
+}
---
+++ src/test/java/org/eoa/projectbudget/service/cache/CacheServiceImplTest.java
@@ -0,0 +1,47 @@
+package org.eoa.projectbudget.service.cache;
+
+import org.eoa.projectbudget.exception.EoaException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+
+
+/**
+ * @Author: 张骏山
+ * @Date: 2023/10/30 14:58
+ * @PackageName: org.eoa.projectbudget.service.cache
+ * @ClassName: CacheServiceImplTest
+ * @Description: redis缓存测试类
+ * @Version 1.0
+ **/
+
+@SpringBootTest
+class CacheServiceImplTest {
+
+    @Autowired
+    CacheServiceImpl cacheService;
+
+    @Test
+    void all() throws EoaException, InterruptedException {
+        cacheService.flashFlagCache("Test");
+        Date date = new Date();
+        Thread.sleep(1000);
+        cacheService.setCache("Test","test1","test1",new Date());
+        System.out.println(cacheService.getCache("Test","test1","test1",date,Date.class));
+        Thread.sleep(100);
+        cacheService.setCache("Test","test1","test2",new Date());
+        System.out.println(cacheService.getCache("Test","test1","test2",date,Date.class));
+        Thread.sleep(100);
+        cacheService.setCache("Test","test1","test2",new Date());
+        System.out.println(cacheService.getCache("Test","test1","test2",date,Date.class));
+        Thread.sleep(100);
+        cacheService.flashFlagCache("Test");
+        System.out.println(cacheService.getCache("Test","test1","test2",date,Date.class));
+        Thread.sleep(100);
+    }
+
+
+}
