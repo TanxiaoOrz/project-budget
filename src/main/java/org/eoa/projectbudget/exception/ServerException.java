7bfd9355dad20e118c5b17e1c66e84f7e31d6db5
Tanxiao <13671985248@163.com>
2023-10-30 15:47:05 +0800

    新建系统运行错误

---
+++ src/main/java/org/eoa/projectbudget/exception/ServerException.java
@@ -0,0 +1,19 @@
+package org.eoa.projectbudget.exception;
+
+import org.eoa.projectbudget.vo.Vo;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2023/10/30 14:49
+ * @PackageName: org.eoa.projectbudget.exception
+ * @ClassName: ServerException
+ * @Description: 系统运行异常类
+ * @Version 1.0
+ **/
+public class ServerException extends EoaException{
+    public ServerException(String description,Exception e) {
+        e.printStackTrace();
+        code=Vo.SERVER_ERROR;
+        this.description = description+"\n"+e.getMessage();
+    }
+}
