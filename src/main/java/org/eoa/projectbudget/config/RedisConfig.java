c095d11b70a4617f4aecd2552473c5ed21551df4
Tanxiao <13671985248@163.com>
2023-10-30 14:32:24 +0800

    添加redis配置和缓存目录

---
+++ src/main/java/org/eoa/projectbudget/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package org.eoa.projectbudget.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2023/10/30 13:51
+ * @PackageName: org.eoa.projectbudget.config
+ * @ClassName: RedisConfig
+ * @Description: redis配置类
+ * @Version 1.0
+ **/
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        // 设置key序列化方式string，RedisSerializer.string() 等价于 new StringRedisSerializer()
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式json，使用GenericJackson2JsonRedisSerializer替换默认序列化，RedisSerializer.json() 等价于 new GenericJackson2JsonRedisSerializer()
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        // 使配置生效
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+}
