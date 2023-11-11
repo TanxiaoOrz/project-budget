d4863d5a7a8d6598a0f5f19565aa4961f32236f3
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-11 23:36:43 +0800

    CheckParameter 新增传入结构体公共接口

---
+++ src/main/java/org/eoa/projectbudget/vo/in/CheckParameter.java
@@ -0,0 +1,17 @@
+package org.eoa.projectbudget.vo.in;
+
+import org.eoa.projectbudget.exception.ParameterException;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2023/11/11 22:53
+ * @PackageName: IntelliJ IDEA
+ * @ClassName: CheckParameter
+ * @Description: 传入结构体完成检查接口
+ * @Version: 1.0
+ */
+public interface CheckParameter {
+    void checkSelf(int type) throws ParameterException;
+
+    void checkSelf() throws ParameterException;
+}
