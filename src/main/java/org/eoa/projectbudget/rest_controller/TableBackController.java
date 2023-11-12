f98aec44a9a7fe0599910be07613eb26e3ef1519
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-12 23:22:48 +0800

    TableBackController 增加用户验证

--- src/main/java/org/eoa/projectbudget/rest_controller/TableBackController.java
+++ src/main/java/org/eoa/projectbudget/rest_controller/TableBackController.java
@@ -2,15 +2,13 @@ package org.eoa.projectbudget.rest_controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.eoa.projectbudget.dto.HumanDto;
 import org.eoa.projectbudget.service.table_module.ModuleService;
 import org.eoa.projectbudget.utils.CreateOutUtils;
 import org.eoa.projectbudget.vo.out.ModuleOut;
 import org.eoa.projectbudget.vo.out.Vo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,8 +26,8 @@ public class TableBackController {
 
     @Operation(summary = "获取所有模块信息")
     @GetMapping("")
-    public Vo<List<ModuleOut>> getAllModule() {
-        return new Vo<>(createOutUtils.moduleOuts(moduleService.getAll(1L)));
+    public Vo<List<ModuleOut>> getAllModule(@RequestAttribute("HumanDto") HumanDto humanDto) {
+        return new Vo<>(createOutUtils.moduleOuts(moduleService.getAll(humanDto.getDataId())));
     }
 
 }
