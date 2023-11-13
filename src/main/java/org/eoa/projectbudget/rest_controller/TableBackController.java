c73b21bcc8467a45ed8484b15fff9e365485cd07
Tanxiao <13671985248@163.com>
2023-11-13 17:38:28 +0800

    TableBackController 调整url,增加请求接口

--- src/main/java/org/eoa/projectbudget/rest_controller/TableBackController.java
+++ src/main/java/org/eoa/projectbudget/rest_controller/TableBackController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.eoa.projectbudget.dto.HumanDto;
 import org.eoa.projectbudget.service.table_module.ModuleService;
 import org.eoa.projectbudget.utils.CreateOutUtils;
+import org.eoa.projectbudget.vo.in.ModuleIn;
 import org.eoa.projectbudget.vo.out.ModuleOut;
 import org.eoa.projectbudget.vo.out.Vo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,8 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/module/back")
-@Tag(name = "模块应用后端接口")
+@RequestMapping("/api/v1/table/back")
+@Tag(name = "模块应用后端接口",description = "需要拥有表单权限的登录用户进行操作")
 @CrossOrigin
 public class TableBackController {
     @Autowired
@@ -25,9 +26,16 @@ public class TableBackController {
     CreateOutUtils createOutUtils;
 
     @Operation(summary = "获取所有模块信息")
-    @GetMapping("")
+    @GetMapping("/module")
     public Vo<List<ModuleOut>> getAllModule(@RequestAttribute("HumanDto") HumanDto humanDto) {
         return new Vo<>(createOutUtils.moduleOuts(moduleService.getAll(humanDto.getDataId())));
+    }
+
+    @Operation(summary = "创建模块应用")
+    @PostMapping("/module")
+    public Vo<String> newModule(@RequestAttribute("HumanDto") HumanDto humanDto, @RequestBody ModuleIn moduleIn) {
+        // TODO
+        return new Vo<>("");
     }
 
 }
