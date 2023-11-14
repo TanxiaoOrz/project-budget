8ce7988649438b19e2b56e6218163842d617d2ab
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-14 20:32:55 +0800

    TableBackController 完成module增删查改接口声明

--- src/main/java/org/eoa/projectbudget/rest_controller/TableBackController.java
+++ src/main/java/org/eoa/projectbudget/rest_controller/TableBackController.java
@@ -1,7 +1,10 @@
 package org.eoa.projectbudget.rest_controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.ibatis.annotations.Delete;
 import org.eoa.projectbudget.dto.HumanDto;
 import org.eoa.projectbudget.service.table_module.ModuleService;
 import org.eoa.projectbudget.utils.CreateOutUtils;
@@ -31,9 +34,36 @@ public class TableBackController {
         return new Vo<>(createOutUtils.moduleOuts(moduleService.getAll(humanDto.getDataId())));
     }
 
+    @Operation(summary = "获取所有模块信息")
+    @GetMapping("/module/{id}")
+    public Vo<ModuleOut> getModule(@RequestAttribute("HumanDto") HumanDto humanDto,
+                                   @PathVariable("id") Long id) {
+        return new Vo<>(null);
+    }
+
     @Operation(summary = "创建模块应用")
     @PostMapping("/module")
-    public Vo<String> newModule(@RequestAttribute("HumanDto") HumanDto humanDto, @RequestBody ModuleIn moduleIn) {
+    public Vo<String> newModule(@RequestAttribute("HumanDto") HumanDto humanDto,
+                                @RequestBody ModuleIn moduleIn) {
+        // TODO
+        return new Vo<>("");
+    }
+
+    @Operation(summary = "修改模块描述")
+    @PutMapping("/module/{id}")
+    @Parameter(name = "id",description = "数据编号",required = true,in = ParameterIn.PATH)
+    public Vo<String> updateModule(@RequestAttribute("HumanDto") HumanDto humanDto,
+                                   @RequestBody ModuleIn moduleIn,
+                                   @PathVariable("id")Long id) {
+        //TODO
+        return new Vo<>("");
+    }
+
+    @Operation(summary = "删除应用")
+    @DeleteMapping("/module/{id}")
+    @Parameter(name = "id",description = "数据编号",required = true,in = ParameterIn.PATH)
+    public Vo<String> deleteModule(@RequestAttribute("HumanDto") HumanDto humanDto,
+                                   @PathVariable("id") Long id) {
         // TODO
         return new Vo<>("");
     }
