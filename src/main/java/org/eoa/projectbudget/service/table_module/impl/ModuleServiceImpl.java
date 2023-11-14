79579cfada6f34fdd80cb554dd498ecf37814a4c
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-14 20:33:19 +0800

    ModuleServiceImpl 增加日志

--- src/main/java/org/eoa/projectbudget/service/table_module/impl/ModuleServiceImpl.java
+++ src/main/java/org/eoa/projectbudget/service/table_module/impl/ModuleServiceImpl.java
@@ -4,6 +4,8 @@ import org.eoa.projectbudget.entity.ModuleType;
 import org.eoa.projectbudget.mapper.ModuleViewMapper;
 import org.eoa.projectbudget.entity.ModuleView;
 import org.eoa.projectbudget.service.table_module.ModuleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,14 @@ public class ModuleServiceImpl implements ModuleService {
     @Autowired
     ModuleViewMapper mapper;
 
+    private final Logger log = LoggerFactory.getLogger("TableModule");
+
 
     @Override
     public List<ModuleView> getAll(Long userId) {
-        return mapper.selectList(null);
+        List<ModuleView> moduleViews = mapper.selectList(null);
+        log.info("用户=>{}查询模块列表",userId);
+        return moduleViews;
     }
 
     @Override
