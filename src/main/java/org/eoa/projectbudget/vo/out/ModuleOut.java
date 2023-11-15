50eb9714925e6562b052c9d3791f3915efb835bc
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-15 21:12:44 +0800

    完成ModuleOut的注释

--- src/main/java/org/eoa/projectbudget/vo/out/ModuleOut.java
+++ src/main/java/org/eoa/projectbudget/vo/out/ModuleOut.java
@@ -13,7 +13,7 @@ import java.util.Date;
  * @Date: 2023/10/21 20:56
  * @PackageName: org.eoa.projectbudget.vo
  * @ClassName: ModuleOut
- * @Description: TODO
+ * @Description: 应用模块显示类
  * @Version 1.0
  **/
 
@@ -24,15 +24,25 @@ import java.util.Date;
 @Schema(name = "ModuleOut", description = "应用模块显示类")
 public class ModuleOut {
 
+    @Schema(description = "唯一id")
     private Long moduleTypeId;
+    @Schema(description = "应用名称")
     private String moduleTypeName;
+    @Schema(description = "应用备注")
     private String workflowRemark;
+    @Schema(description = "创建者编号")
     private Long creatorId;
+    @Schema(description = "创建者姓名")
     private String creatorName;
+    @Schema(description = "创建时间")
     private Date createTime;
+    @Schema(description = "查询数量")
     private Integer searchCounts;
+    @Schema(description = "流程数量")
     private Integer flowCounts;
+    @Schema(description = "表单数量")
     private Integer tableCounts;
+    @Schema(description = "图标数量")
     private Integer chartsCounts;
 
 }
