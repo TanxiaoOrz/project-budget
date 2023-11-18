1835033530827c3d8a0ddd905842bf2f7ee1707d
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-19 00:43:04 +0800

    测试wrapper的输出对象

--- src/test/java/org/eoa/projectbudget/mapper/ColumnViewMapperTest.java
+++ src/test/java/org/eoa/projectbudget/mapper/ColumnViewMapperTest.java
@@ -1,10 +1,10 @@
 package org.eoa.projectbudget.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.eoa.projectbudget.entity.ColumnView;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ColumnViewMapperTest {
@@ -15,5 +15,18 @@ class ColumnViewMapperTest {
     void test() {
         System.out.println(columnViewMapper.selectList(null));
         System.out.println(columnViewMapper.getColumnViewNoNew(1L));
+    }
+
+    @Test
+    void toLogString() {
+        QueryWrapper<ColumnView> wrapper= new QueryWrapper<>();
+        wrapper.eq("columnId",1).between("name",1,2);
+        System.out.println("wrapper.getCustomSqlSegment() = " + wrapper.getCustomSqlSegment());
+        System.out.println("wrapper.getTargetSql() = " + wrapper.getTargetSql());
+        System.out.println("wrapper.getSqlSet() = " + wrapper.getSqlSet());
+        System.out.println("wrapper.getSqlComment() = " + wrapper.getSqlComment());
+        System.out.println("wrapper.getSqlSegment() = " + wrapper.getSqlSegment());
+        System.out.println("wrapper.getSqlFirst() = " + wrapper.getSqlFirst());
+        System.out.println("wrapper.getParamNameValuePairs().toString() = " + wrapper.getParamNameValuePairs().toString());
     }
 }
