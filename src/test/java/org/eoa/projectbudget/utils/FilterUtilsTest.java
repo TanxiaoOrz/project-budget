fd411c8036cb4acdbdf420c1187c072db5c9bcbc
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-19 00:43:39 +0800

    测试StringBuilder的空ToString

---
+++ src/test/java/org/eoa/projectbudget/utils/FilterUtilsTest.java
@@ -0,0 +1,22 @@
+package org.eoa.projectbudget.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2023/11/18 23:41
+ * @PackageName: IntelliJ IDEA
+ * @ClassName: FilterUtilsTest
+ * @Description: TODO
+ * @Version: 1.0
+ */
+class FilterUtilsTest {
+
+    @Test
+    void getDescription() {
+        String s = new StringBuilder().toString();
+        System.out.println(s);
+    }
+}
