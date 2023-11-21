42e9e2fa1e1100834db5b1eac2be037eeeedf353
Tanxiao <13671985248@163.com>
2023-11-21 16:16:50 +0800

    TableView 注释修改

--- src/main/java/org/eoa/projectbudget/entity/TableView.java
+++ src/main/java/org/eoa/projectbudget/entity/TableView.java
@@ -15,8 +15,8 @@ import java.util.Date;
  * @Author 张骏山
  * @Date 2023/10/10 16:48
  * @PackageName: org.eoa.projectbudget.entity
- * @ClassName: TableOut
- * @Description: TODO
+ * @ClassName: TableView
+ * @Description: 虚拟视图实体类
  * @Version 1.0
  */
 @EqualsAndHashCode(callSuper = false)
