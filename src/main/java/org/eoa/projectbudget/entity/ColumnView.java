3a41a87e71b3bc2d4f0d3ec375c79b11a87a0dcc
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-23 21:49:38 +0800

    ColumnView.columnViewDisplay 属性类型修改

--- src/main/java/org/eoa/projectbudget/entity/ColumnView.java
+++ src/main/java/org/eoa/projectbudget/entity/ColumnView.java
@@ -1,15 +1,11 @@
 package org.eoa.projectbudget.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
-
-import java.util.Date;
 
 /**
  * @Author 张骏山
@@ -28,6 +24,6 @@ import java.util.Date;
 @TableName("table_view_column_index")
 public class ColumnView extends Column{
 
-    private String columnViewDisplay;
+    private Integer columnViewDisplay;
 
 }
