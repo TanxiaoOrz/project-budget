c3124ae3c267e26b3fb1f20aecf454e621ffd9b0
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-12-16 23:13:37 +0800

    DepartMapper.java SectionMapper.java 增加连锁废弃的相关方法

--- src/main/java/org/eoa/projectbudget/mapper/DepartMapper.java
+++ src/main/java/org/eoa/projectbudget/mapper/DepartMapper.java
@@ -2,6 +2,8 @@ package org.eoa.projectbudget.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 import org.eoa.projectbudget.entity.Depart;
 
 /**
@@ -15,4 +17,9 @@ import org.eoa.projectbudget.entity.Depart;
 
 @Mapper
 public interface DepartMapper extends BaseMapper<Depart> {
+    @Update("update depart_resource set isDeprecated = 1 where belongDepart = #{belongDepart}")
+    Integer dropBelongDepart(@Param("belongDepart")Long belongDepart);
+
+    @Update("update depart_resource set isDeprecated = 1 where belongSection = #{belongSection}")
+    Integer dropBelongSection(@Param("belongSection")Long belongSection);
 }
--- src/main/java/org/eoa/projectbudget/mapper/SectionMapper.java
+++ src/main/java/org/eoa/projectbudget/mapper/SectionMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.eoa.projectbudget.entity.Section;
 
 /**
@@ -21,4 +22,6 @@ public interface SectionMapper extends BaseMapper<Section> {
     @Select("select belongSection from  section_resource where dataId = #{dataId}")
     Long getBelong(@Param("dataId")Long dataId);
 
+    @Update("update section_resource set isDeprecated = 1 where belongSection = #{belongSection}")
+    Integer dropBelongDepart(@Param("belongSection")Long belongId);
 }
