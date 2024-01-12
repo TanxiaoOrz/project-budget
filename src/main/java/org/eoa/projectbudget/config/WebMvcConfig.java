8f1849e3b864e60319b2b823551e1c7e2d04209f
Tanxiao <13671985248@163.com>
2024-01-12 17:45:36 +0800

    WebMvcConfig.java 增加组织,流程,展示后端接口校验

--- src/main/java/org/eoa/projectbudget/config/WebMvcConfig.java
+++ src/main/java/org/eoa/projectbudget/config/WebMvcConfig.java
@@ -29,5 +29,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/v1/content/back/**");
         registry.addInterceptor(new BackInterceptor(new Long[]{5L},"操作角色模块","需要权限模块操作权限,权限id=>5,请联系相关管理员添加"))
                 .addPathPatterns("/api/v1/authority/back/**");
+        registry.addInterceptor(new BackInterceptor(new  Long[]{7L},"操作流程模块","需要流程模块操作权限,权限id=>7,请联系相关管理员添加"))
+                .addPathPatterns("/api/v1/workflow/back");
+        registry.addInterceptor(new BackInterceptor(new  Long[]{3L},"操作组织模块","需要组织模块操作权限,权限id=>3,请联系相关管理员添加"))
+                .addPathPatterns("/api/v1/workflow/back");
+        registry.addInterceptor(new BackInterceptor(new  Long[]{9L},"操作展示模块","需要展示模块操作权限,权限id=>9,请联系相关管理员添加"))
+                .addPathPatterns("/api/v1/workflow/back");
     }
 }
