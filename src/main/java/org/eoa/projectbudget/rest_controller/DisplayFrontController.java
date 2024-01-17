2ee13a299869d9f1303def6a9f8b9854247aa6ae
Tanxiao <13671985248@163.com>
2024-01-17 15:49:20 +0800

    DisplayFrontController.getSearchList 增加权限检测

--- src/main/java/org/eoa/projectbudget/rest_controller/DisplayFrontController.java
+++ src/main/java/org/eoa/projectbudget/rest_controller/DisplayFrontController.java
@@ -3,9 +3,15 @@ package org.eoa.projectbudget.rest_controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.eoa.projectbudget.dto.HumanDto;
+import org.eoa.projectbudget.dto.SearchListDto;
+import org.eoa.projectbudget.dto.constraint.Constraint;
+import org.eoa.projectbudget.entity.SearchList;
+import org.eoa.projectbudget.exception.AuthorityException;
 import org.eoa.projectbudget.exception.EoaException;
 import org.eoa.projectbudget.service.cache.CacheService;
 import org.eoa.projectbudget.service.display.SearchService;
+import org.eoa.projectbudget.service.organization_module.OrganizationService;
+import org.eoa.projectbudget.utils.AuthorityUtils;
 import org.eoa.projectbudget.utils.ChangeFlagUtils;
 import org.eoa.projectbudget.utils.factory.SearchListDtoOutFactory;
 import org.eoa.projectbudget.vo.out.SearchListDtoOut;
@@ -35,6 +41,8 @@ public class DisplayFrontController {
 
     @Autowired
     SearchListDtoOutFactory searchListDtoOutFactory;
+    @Autowired
+    OrganizationService organizationService;
 
     private final long USER_ID_CACHE = 0L;
 
@@ -42,12 +50,25 @@ public class DisplayFrontController {
     @Operation(summary = "获取列表构造体")
     public Vo<SearchListDtoOut> getSearchList(@RequestAttribute("HumanDto") HumanDto humanDto,
                                            @PathVariable Long id) throws EoaException {
-        SearchListDtoOut out = cacheService.getCache(ChangeFlagUtils.SEARCH_DTO,id.toString(), USER_ID_CACHE, SearchListDtoOut.class);
-        if (out == null) {
-            out = searchListDtoOutFactory.out(searchService.getSearchListDto(id, humanDto.getDataId()));
-            cacheService.setCache(ChangeFlagUtils.SEARCH_DTO,id.toString(),USER_ID_CACHE,out);
+        Boolean viewAble = cacheService.getCache(ChangeFlagUtils.SEARCH_DTO, id.toString(), humanDto.getDataId(), Boolean.class);
+        SearchListDto searchListDto = null;
+        if (viewAble == null) {
+            searchListDto =  searchService.getSearchListDto(id, humanDto.getDataId());
+            SearchList searchList = searchListDto.getSearchList();
+            Constraint constraint = AuthorityUtils.getConstraint(searchList.getShareAuthority());
+            viewAble = AuthorityUtils.checkAuthority(humanDto,organizationService.getHumanDto(searchList.getCreator(),null),constraint);
         }
-        return new Vo<>(out);
+        if (viewAble) {
+            SearchListDtoOut out = cacheService.getCache(ChangeFlagUtils.SEARCH_DTO, id.toString(), USER_ID_CACHE, SearchListDtoOut.class);
+            if (out == null) {
+                searchListDto =searchListDto !=null?searchListDto: searchService.getSearchListDto(id, humanDto.getDataId());
+                out = searchListDtoOutFactory.out(searchListDto);
+                cacheService.setCache(ChangeFlagUtils.SEARCH_DTO, id.toString(), USER_ID_CACHE, out);
+            }
+            return new Vo<>(out);
+        } else {
+            throw new AuthorityException(humanDto.getDataId(), "展示列表", id, "查看");
+        }
 
     }
 
