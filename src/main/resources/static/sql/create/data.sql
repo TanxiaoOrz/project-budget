dbfddac81001ab376fa3ed038dc6c6fd5f985d6e
Tanxiao <13671985248@163.com>
2024-01-25 17:41:55 +0800

    data.sql 添加部分期初数据

--- src/main/resources/static/sql/create/data.sql
+++ src/main/resources/static/sql/create/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO `human_resource` (dataId, loginName, password, name, sex, birth, telephone, mail, phone, fax, workCode,
-                              section, depart, job, directorLeader, supporter, photo, signature, lastLogin,safety)
+                              section, depart, job, directorLeader, supporter, photo, signature, lastLogin, safety)
 VALUES (1, 'sysadmin', 'eoa', '系统管理员', null, null, null, null, null, null, null, 0, 0, '系统管理员', 0, 0, 0, null,
-        null,0),
-       (2, 'tourist', 'eoa', '游客用户', null, null, null, null, null, null, null, 0, 0, '游客', 1, 0, 0, null, null,0);
+        null, 0),
+       (2, 'tourist', 'eoa', '游客用户', null, null, null, null, null, null, null, 0, 0, '游客', 1, 0, 0, null, null, 0);
 -- 添加管理员
 
 
@@ -61,88 +61,219 @@ values ('1', '1');
 Insert Into `content_list` (dataId, contentName, contentRemark, creator, createTime, defaultEdit, defaultCreate,
                             defaultDelete, defaultShare, leadContent)
 VALUES (1, '组织架构展示资料', '系统默认目录,勿动', 1, now(), null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', 0),
+  "bodyType": "allConstraint",
+  "body": {
+    "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+  },
+  "tableType": null,
+  "table": null
+}', null, '{
+  "bodyType": "allConstraint",
+  "body": {
+    "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+  },
+  "tableType": null,
+  "table": null
+}', 0),
        (2, '人员头像', '系统默认目录,勿动', 1, now(), null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', 1),
+         "bodyType": "allConstraint",
+         "body": {
+           "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+         },
+         "tableType": null,
+         "table": null
+       }', null, '{
+         "bodyType": "allConstraint",
+         "body": {
+           "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+         },
+         "tableType": null,
+         "table": null
+       }', 1),
        (3, '部门头像', '系统默认目录,勿动', 1, now(), null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', 1),
+         "bodyType": "allConstraint",
+         "body": {
+           "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+         },
+         "tableType": null,
+         "table": null
+       }', null, '{
+         "bodyType": "allConstraint",
+         "body": {
+           "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+         },
+         "tableType": null,
+         "table": null
+       }', 1),
        (4, '分部头像', '系统默认目录,勿动', 1, now(), null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', null, '{
-     "bodyType":"allConstraint",
-     "body":{
-       "allConstraint":"{\\"start\\":0,\\"end\\":100}"
-     },
-     "tableType":null,
-     "table":null
-   }', 1);
+         "bodyType": "allConstraint",
+         "body": {
+           "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+         },
+         "tableType": null,
+         "table": null
+       }', null, '{
+         "bodyType": "allConstraint",
+         "body": {
+           "allConstraint": "{\\"start\\":0,\\"end\\":100}"
+         },
+         "tableType": null,
+         "table": null
+       }', 1);
 
 -- 前端目录默认配置
-    INSERT
+INSERT
 INTO `menu_base` (`dataId`, `contentName`, `belongContent`, `contentUrl`, `viewNo`, `isDeprecated`, `shareAuthority`,
                   `creator`, `createTime`)
-VALUES (1, '主页', NULL, NULL, 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (2, '组织结构', NULL, NULL, 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (3, '知识目录', NULL, NULL, 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (4, '工作流程', NULL, NULL, 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (5, '公司主页', 1, '/main', 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (6, '组织结构树', 2, '/organization', 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (7, '分部列表', 2, '/section', 2, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (8, '部门列表', 2, '/depart', 3, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (9, '人员列表', 2, '/human_resource', 4, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (10, '知识地图', 3, '/content/0', 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (11, '待办请求', 4, '/request/backlog', 1, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (12, '已办请求', 4, '/request/done', 2, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (13, '我的请求', 4, '/request/self', 3, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now())
-        , (14, '发起请求', 4, '/workflow', 4, 0, '{\"body\": {\"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"}, \"table\": {}, \"bodyType\": \"allConstraint,\", \"tableType\": \"\"}', 1, now());
+VALUES (1, '主页', NULL, NULL, 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (2, '组织结构', NULL, NULL, 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (3, '知识目录', NULL, NULL, 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (4, '工作流程', NULL, NULL, 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (5, '公司主页', 1, '/main', 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (6, '组织结构树', 2, '/organization', 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (7, '分部列表', 2, '/section', 2, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (8, '部门列表', 2, '/depart', 3, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (9, '人员列表', 2, '/human_resource', 4, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (10, '知识地图', 3, '/content/0', 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (11, '待办请求', 4, '/request/backlog', 1, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (12, '已办请求', 4, '/request/done', 2, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (13, '我的请求', 4, '/request/self', 3, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now())
+     , (14, '发起请求', 4, '/workflow', 4, 0, '{
+  \"body\": {
+    \"allConstraint\": \"{\\\"start\\\":0,\\\"end\\\":100}\"
+  },
+  \"table\": {},
+  \"bodyType\": \"allConstraint,\",
+  \"tableType\": \"\"
+}', 1, now());
 
-INSERT INTO `eoa_build`.`login_config` (`dataId`, `backgroundUrl`, `logoUrl`, `backgroundVideoUrl`, `loginTitle`, `loginSubTitle`, `activeMainTitle`, `activeIntroduction`, `contactManager`, `linkUrl`, `linkStr`, `creator`, `createTime`, `onUse`) VALUES ('1', 'https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*y0ZTS6WLwvgAAAAAAAAAAAAADml6AQ/fmt.webp', 'https://github.githubassets.com/images/modules/logos_page/Octocat.png', 'https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr', 'EOA', '可配置的低代码办公平台', '项目预算特化开发版本', '作者相关', '张骏山:13671985248', 'http://127.0.0.1:8080/doc.html', '查看', '1', now(), '1');
+INSERT INTO `eoa_build`.`login_config` (`dataId`, `backgroundUrl`, `logoUrl`, `backgroundVideoUrl`, `loginTitle`,
+                                        `loginSubTitle`, `activeMainTitle`, `activeIntroduction`, `contactManager`,
+                                        `linkUrl`, `linkStr`, `creator`, `createTime`, `onUse`)
+VALUES ('1', 'https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*y0ZTS6WLwvgAAAAAAAAAAAAADml6AQ/fmt.webp',
+        'https://github.githubassets.com/images/modules/logos_page/Octocat.png',
+        'https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr', 'EOA',
+        '可配置的低代码办公平台', '项目预算特化开发版本', '作者相关', '张骏山:13671985248', 'http://127.0.0.1:8080/doc.html', '查看', '1', now(),
+        '1');
 
-INSERT INTO `eoa_build`.`page_config` (`dataId`, `companyName`, `headerColor`, `sideColor`, `onUse`, `creator`, `createTime`) VALUES ('1', '业务流办公平台-项目预算', '#140066', '#8A8DAD', '1', '1',NOW());
+INSERT INTO `eoa_build`.`page_config` (`dataId`, `companyName`, `headerColor`, `sideColor`, `onUse`, `creator`,
+                                       `createTime`)
+VALUES ('1', '业务流办公平台-项目预算', '#140066', '#8A8DAD', '1', '1', NOW());
 
 
+INSERT INTO `table_view_index` (`tableId`, `tableViewName`, `tableDataName`, `moduleNo`, `groupCount`, `groupName`,
+                                `remark`, `creator`, `createTime`)
+VALUES (1, '人力资源', 'human_resource', 1, 2, '人力信息,联系方式', '系统默认索引表单', 1, '2024-01-24 13:39:39')
+     , (2, '职能部门', 'depart_resource', 1, 1, '组织信息', '系统默认索引表单', 1, '2024-01-24 17:10:35')
+     , (3, '公司分部', 'section_resource', 1, 1, '分部信息', '系统默认索引表单', 1, '2024-01-24 17:26:21')
+     , (4, '目录', 'content_list', 2, 1, '目录信息', '系统默认索引表单', 1, '2024-01-25 16:25:21')
+     , (5, '文件', 'file_storage', 2, 1, '文件信息', '系统默认索引表单', 1, '2024-01-25 16:27:29');
+
+INSERT INTO `table_view_column_index` (`columnId`, `columnViewName`, `columnDataName`, `columnType`,
+                                       `columnTypeDescription`, `tableNo`, `columnGroupNo`, `columnViewNo`, `creator`,
+                                       `createTime`, `columnViewDisplay`)
+VALUES (1, '姓名', 'name', 'SINGLE_TEXT', NULL, 1, 1, 1, 1, '2024-01-24 14:29:50', 1)
+     , (2, '性别', 'sex', 'SELECT_ITEM', '{\"items\":\"男,女\"}', 1, 1, 2, 1, '2024-01-24 14:45:07', 1)
+     , (3, '手机号', 'phone', 'SINGLE_TEXT', NULL, 1, 2, 3, 1, '2024-01-24 14:46:44', 1)
+     , (4, '部门名称', 'departName', 'SINGLE_TEXT', NULL, 2, 1, 1, 1, '2024-01-24 17:22:31', 1)
+     , (5, '分部名称', 'sectionName', 'SINGLE_TEXT', NULL, 3, 1, 1, 1, '2024-01-24 17:39:56', 1)
+     , (6, '目录名称', 'contentName', 'SINGLE_TEXT', NULL, 4, 1, 1, 1, '2024-01-25 16:26:56', 1)
+     , (7, '文件名称', 'fileName', 'SINGLE_TEXT', NULL, 5, 1, 1, 1, '2024-01-25 16:30:10', 1);
+
+INSERT INTO `section_resource` (`dataId`, `sectionName`, `sectionCode`, `fullName`, `belongSection`, `sectionManager`,
+                                `sectionIntroduction`, `createTime`, `photo`, `isDeprecated`)
+VALUES (1, '支撑服务分部', 'SERVE', '维森化工石油公司支撑服务分部', 0, NULL, NULL, '2024-01-25 17:10:40', NULL, 0),
+       (2, '能源制造公司', 'ENERGY', '维森化工石油-能源制造公司', 0, NULL, NULL, '2024-01-25 17:19:26', NULL, 0),
+       (3, '设备制造分部', 'EQUIPMENT', '维森化工石油-设备制造分部', 0, NULL, NULL, '2024-01-25 17:26:51', NULL, 0);
 
